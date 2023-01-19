6893a0c4273bd377e6adf2d2e7f9c280c821586e
purnachandra057 <lightvelocity888@gmail.com>
2023-01-19 12:21:58 +0545

    Add async wrapper for error handling

--- app/index.js
+++ app/index.js
@@ -2,16 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const errorHandler = require('../middlewares/errorHandler')
 const { PrismaClient } = require('@prisma/client')
+const asyncWrapper = require('../lib/asyncWrapper')
 
 const app = express()
 const prisma = new PrismaClient()
 
 app.use(bodyParser.json())
 
-app.get('/', async (req, res, next) => {
-    const userCount = await prisma.user.count()
-    res.json({ userCount })
-})
+app.get(
+    '/',
+    asyncWrapper(async (req, res, next) => {
+        const userCount = await prisma.user.count()
+        res.json({ userCount })
+    })
+)
 
 app.use(errorHandler)
 
