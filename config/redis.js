ca5ac311981c9929c404b8c3cd82273614d61f60
purnachandra057 <lightvelocity888@gmail.com>
2023-05-16 14:06:08 +0545

    Modification in redis configuration

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,11 @@
 const Redis = require('redis')
 
-const redisClient = Redis.createClient({ url: process.env.REDIS_URL })
+let options
+
+if (process.env.REDIS_URL) {
+    options = { url: process.env.REDIS_URL }
+}
+
+const redisClient = Redis.createClient(options)
 
 module.exports = redisClient
